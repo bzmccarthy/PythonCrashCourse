76bd85e158d243a5a838596c217ec44f05ce2b00
bzmccarthy <bzmccarthy@gmail.com>
2022-05-05 08:01:06 -0400

    Ch2 continuing work

---
+++ Chapter2/numbers.py
@@ -0,0 +1,13 @@
+print(2*5)
+print(3/2)
+print(3//2)
+print(0.2 + 0.1)
+print(10/2)
+
+universe_age = 14_000_000_000
+
+print(universe_age)
+
+x, y , z = 0, 0, 0
+
+print(x, y, z)
---
+++ Chapter2/personal_message.py
@@ -0,0 +1,6 @@
+raw_name = input('What is your name?')
+
+print(f'Hello, {raw_name} would you like to learn Python today?')
+
+print(f'Your name in uppercase is {raw_name.upper()}\nYour name in lowercase is {raw_name.lower()}\nYour name in title case is {raw_name.title()}')
+
