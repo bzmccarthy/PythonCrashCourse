6e250d68494d2d9a15fac4cdcf6b8c797935dca4
Brian McCarthy <bzmccarthy@gmail.com>
2022-05-18 07:51:39 -0400

    More Ch4

---
+++ Chapter4/dimensions.py
@@ -0,0 +1,14 @@
+dimensions = (200, 50,
+              50)
+
+print(dimensions[0])
+
+for dimension in dimensions:
+    print(dimension)
+    
+dim_list = [1,2,3,
+            4,5,6,
+            7,8,9]
+
+for dim in dim_list:
+    print(dim)
