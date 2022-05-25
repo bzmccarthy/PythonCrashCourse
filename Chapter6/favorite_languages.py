312634d10c16d8152bf70fbfcec1fba118184f6d
Brian McCarthy <bzmccarthy@gmail.com>
2022-05-25 07:55:37 -0400

    Ch6 continued

--- Chapter6/favorite_languages.py
+++ Chapter6/favorite_languages.py
@@ -4,3 +4,28 @@ favorite_languages = {
     'edward': 'ruby',
     'phil': 'python',    
     }
+
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
+
+for name in favorite_languages.keys():
+    print(name.title())
+    
+friends = ['phil', 'sarah']
+
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+        
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+    
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you for taking the poll.")
+    
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
