75e711a23ce112dbf371062aeb67b8196e92d19b
Brian McCarthy <bzmccarthy@gmail.com>
2022-05-31 08:01:15 -0400

    Ch8 continued

--- Chapter8/greeter.py
+++ Chapter8/greeter.py
@@ -1,6 +1,19 @@
-def greet_user(username):
-    """"Display a simple greeting."""
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+while True:
+    print("\n(enter 'q' at any time to quit)")
+    print("\nPlease tell me your name: ")
     
-    print(f"Hello, {username.title()}!")
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
     
-greet_user('Bob')
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+    
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
--- Chapter8/pets.py
+++ Chapter8/pets.py
@@ -1,7 +1,11 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name, animal_type ='dog'):
     """Display information about a pet."""
     
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
     
-describe_pet('alligator', 'Bonnie')
+describe_pet(pet_name='willie')
+
+describe_pet('toby')
+
+describe_pet(pet_name='harry', animal_type='hamster')
