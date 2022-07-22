5853ce3e63f10fab531c1327fbe609f66412eb30
Brian McCarthy <bzmccarthy@gmail.com>
2022-07-22 07:52:09 -0400

    Alien invasion continued

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -10,7 +10,7 @@ class Settings:
         
         # Ship settings
         self.ship_speed = 0.3
-        self.ship_limit = 3
+        self.ship_limit = 1
         
         # Bullet settings
         self.bullet_speed = 1.5
@@ -20,7 +20,7 @@ class Settings:
         self.bullets_allowed = 3
         
         # Alien settings
-        self.alien_speed = 0.2
+        self.alien_speed = 0.4
         self.fleet_drop_speed = 15
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
