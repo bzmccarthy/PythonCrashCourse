d89b2926b08fb38886d5032ac59559dd02ca92d6
Brian McCarthy <bzmccarthy@gmail.com>
2022-08-11 08:00:17 -0400

    Ch15 continued

--- Chapter15/rw_visual.py
+++ Chapter15/rw_visual.py
@@ -6,16 +6,18 @@ from random_walk import RandomWalk
 while True:
 
     # Make a random walk
-    rw = RandomWalk(num_points=100_000)
+    rw = RandomWalk(num_points=50_000)
     rw.fill_walk()
     
     # Plot the points in the walk
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(9, 5))
     
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
                edgecolors='none', s=1)
+    
+    # ax.plot(rw.x_values, rw.y_values, linewidth = 1)
     
     # Emphasize the first and last points
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
