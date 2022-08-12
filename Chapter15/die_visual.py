93f0f8cf28bfe86b63026b67c1938f21d6cd9f06
Brian McCarthy <bzmccarthy@gmail.com>
2022-08-12 08:02:38 -0400

    Ch15 continued

--- Chapter15/die_visual.py
+++ Chapter15/die_visual.py
@@ -1,12 +1,30 @@
 from die import Die
 
+import matplotlib.pyplot as plt
+
 # Create a d6
 die = Die(num_sides=8)
+die2 = Die(num_sides=8)
 
 # Make some rolls, and store results in a list
 results = []
-for roll_num in range(100):
-    result = die.roll()
+
+for roll_num in range(100000):
+    result = die.roll() + die2.roll()
     results.append(result)
     
-print(results)
+# Analyze results
+frequencies = []
+
+for value in range(1+1, die.num_sides+die2.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+    
+# Visualize the results
+x_values = list(range(1+1, die.num_sides+die2.num_sides+2))
+y_values = frequencies
+
+    
+print(frequencies)
+
+plt.hist(x=results, bins = x_values)
