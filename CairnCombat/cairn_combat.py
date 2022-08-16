98d24691d6db44d898c8b252ace9969037d540e7
Brian McCarthy <bzmccarthy@gmail.com>
2022-08-16 08:05:44 -0400

    Cairn combat continued

--- CairnCombat/cairn_combat.py
+++ CairnCombat/cairn_combat.py
@@ -2,7 +2,49 @@ import os
 os.chdir('C:/Users/bzmcc/Documents/PythonCrashCourse/CairnCombat')
 
 from combatant import Combatant
+from die import Die
 
-penelope = Combatant(hp=6, ac=0, hit_die=12,
-                strength=10, dexterity=10, willpower=10,
+
+#PCs
+penelope = Combatant(hp=6, ac=2, hit_die=8,
+                strength=8, dexterity=11, willpower=10,
                 name='Penelope')
+
+bill = Combatant(hp=6, ac=1, hit_die=6,
+                strength=8, dexterity=10, willpower=10,
+                name='Bill')
+
+abobo = Combatant(hp=6, ac=2, hit_die=8,
+                strength=11, dexterity=14, willpower=13,
+                name='Abobo')
+
+mathia = Combatant(hp=6, ac=0, hit_die=6,
+                strength=11, dexterity=13, willpower=13,
+                name='Mathia')
+
+# NPCs
+alfred = Combatant(hp=4, ac=2, hit_die=8,
+                strength=11, dexterity=8, willpower=18,
+                name='Sir Alfred')
+
+
+pc_list = [penelope, bill, abobo, mathia]
+npc_list = [alfred]
+
+def roll_initiative(pc_list):
+    """Roll to see who goes first"""
+    
+    d20 = Die(num_sides=20)
+    
+    for pc in pc_list:
+        d20_roll = d20.roll()
+        if d20_roll <= pc.dexterity:
+            print(f"{pc.name} rolls a {d20_roll} and goes first.")
+        else:
+            print(f"{pc.name} rolls a {d20_roll} and misses initiative.")
+            
+            
+            
+            
+            
+            
--- CairnCombat/combatant.py
+++ CairnCombat/combatant.py
@@ -21,30 +21,67 @@ class Combatant:
         print(f"{self.name} rolled a {attack_roll}.")
         return attack_roll
     
+    def take_damage(self, enemy_attack_roll=0):
+        """See if and how much damage is taken"""
+        
+        actual_damage = enemy_attack_roll - self.ac
+        
+        if actual_damage <= 0:
+            print(f"Enemy only rolled {enemy_attack_roll}.")
+            print(f"Lower than {self.name}'s AC of {self.ac}.")
+            print("No damage taken.")
+        
+        else:
+            self.subtract_hp(actual_damage)
+    
     def subtract_hp(self, damage_taken=0):
         """Subtract hp if combatant is damaged"""
         if damage_taken < self.hp:
             self.hp -= damage_taken
             print(f"{self.name} has taken {damage_taken} damage.")
-            print(f"{self.name} has {self.hp} HP left.")
-            print(f"{self.name} has {self.strength} STR left.")
+            self.print_stats()
+
         elif damage_taken == self.hp:
             # Get a scar!
             print(f"{self.name} gets a scar!")
-            print(f"{self.name} has {self.hp} HP left.")
-            print(f"{self.name} has {self.strength} STR left.")
-            pass
-        else:
+            self.hp -= damage_taken
+            self.print_stats()
+            
+        else:            
             str_damage = damage_taken - self.hp
+            print(f"{self.name} takes {self.hp} HP damage.")
+            print(f"{self.name} takes {str_damage} STR damage.")
             self.hp = 0
+            
             if str_damage >= self.strength:
                 self.strength = 0
                 print(f"{self.name} is dead.")
-                print(f"{self.name} has {self.hp} HP left.")
-                print(f"{self.name} has {self.strength} STR left.")
+                self.print_stats()
+                
             else:
                 self.strength -= str_damage
+                self.print_stats()
                 # Roll for critical damage
                 print(f"{self.name} needs to roll for critical damage.")
-                print(f"{self.name} has {self.hp} HP left.")
-                print(f"{self.name} has {self.strength} STR left.")
+                self.roll_critical()
+                
+    def print_stats(self):
+        """Print the combatants stats"""
+        print(f"{self.name} has {self.hp} HP left.")
+        print(f"{self.name} has {self.strength} STR left.")
+        
+    def attack(self, enemy):
+        """Attack an enemy"""
+        attack_roll = self.attack_die.roll()
+        print(f"{self.name} attacks {enemy.name}, rolls a {attack_roll}.")
+        enemy.take_damage(attack_roll)
+        
+    def roll_critical(self):
+        """Roll for critical damage"""
+        d20_roll = Die(num_sides=20).roll()
+        
+        if d20_roll <= self.strength:
+            print(f"{self.name} has rolled a {d20_roll} and saved!")
+        else:
+            print(f"{self.name} has rolled a {d20_roll}, and is taken out.")
+        
