043b6c285e7180a234e7c48679a8ebb6b1080a2d
Brian McCarthy <bzmccarthy@gmail.com>
2022-08-19 07:56:18 -0400

    Chapter 16 continued

--- Chapter16/stika_highs_lows.py
+++ Chapter16/stika_highs_lows.py
@@ -12,20 +12,26 @@ with open(filename) as f:
     header_row = next(reader)
     
     # Get high temps from this file
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         dates.append(current_date)
+        
         high = int(row[5])
         highs.append(high)
         
-# Plot the high temperatures
+        low = int(row[6])
+        lows.append(low)
+        
+# Plot the high and low temperatures
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot
-ax.set_title("Daily high temperatures - 2018", fontsize=24)
+ax.set_title("Daily high and low temperatures - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=12)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=12)
